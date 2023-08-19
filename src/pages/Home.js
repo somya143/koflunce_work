a1da0f81891ff35c8491e329680c6c0576bb227e
Somya Ranjan Singh <maaheersj@gmail.com>
2023-08-19 07:18:02 +0530

    styling added for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import "./home.css";
 
 const Home = () => {
 
   return (
-    <div>
-      <Link to={"/signup"} >Register</Link>
-      <Link to={"/login"}>Login</Link>
+    <div className='home'>
+      <Link className='signupLink' to={"/signup"} >Register</Link>
+      <Link className='loginLink' to={"/login"}>Login</Link>
+      <Link className='userDetail' to={"/userDetails"} >User Details</Link>
     </div>
   )
 }
